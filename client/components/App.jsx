4784b36305ca72ab3616e2b7ead27b9cfaf6dd15
todddrinkwater <daniel-reason>
2017-05-19 10:54:55 +1200

    added routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,16 +4,17 @@ import {HashRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>Odd One Out</h1>
-    <hr></hr>
-    <div className='content'>
-      <Route exact={true} path='/' component={Home}/>
-      <Route path='/level1' component={Emoji}/>
-      <Route path='/level2' component={Faces}/>
-      <Route path='/level3' component={Dolphins}/>
-      <Route path='/level4' component={Land1}/>
-    </div>
-
+    <Router>
+      <h1>Odd One Out</h1>
+      <hr></hr>
+      <div className='content'>
+        <Route exact={true} path='/' component={Home}/>
+        <Route path='/level1' component={Emoji}/>
+        <Route path='/level2' component={Faces}/>
+        <Route path='/level3' component={Dolphins}/>
+        <Route path='/level4' component={Land1}/>
+      </div>
+    </Router>
   )
 }
 
