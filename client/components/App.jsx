db292bfdee9e86879483d904557c6d6118b5ae13
todddrinkwater <daniel-reason>
2017-05-19 10:51:54 +1200

    working on routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Emoji from './Emoji'
+import {HashRouter as Router, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <h1>Odd One Out</h1>
+    <hr></hr>
+    <div className='content'>
+      <Route exact={true} path='/' component={Home}/>
+      <Route path='/level1' component={Emoji}/>
+      <Route path='/level2' component={Faces}/>
+      <Route path='/level3' component={Dolphins}/>
+      <Route path='/level4' component={Land1}/>
+    </div>
+
   )
 }
 
 export default App
-
---
+++ client/components/Emoji.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Emoji = () => {
+  return (
+    <span>Find the x on the page</span>
+    <span>IMAGES IMAGES HERE</span>
+  )
+}
+
+export default Emoji
