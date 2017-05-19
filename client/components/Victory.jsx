b45c47db4a69359fbfd07e0ea8c7abe0db4753df
Sherly Chan <sherlyc@gmail.com>
2017-05-19 14:57:11 +1200

    added victory

--- client/components/Victory.jsx
+++ client/components/Victory.jsx
@@ -4,6 +4,10 @@ const Victory = () => {
   return (
     <div>
       <h1>Congratulations!</h1>
+          <audio autoPlay>
+          <source src="scream.mp3" type="audio/mpeg"/>
+              Your browser does not support the audio tag.
+         </audio>
       <img id='victory' src='/sherly.jpg'></img>
     </div>
   )
