c0ffb3de0a50ec2f3472d2e318ecbc4b8b06dd21
Thomasin Abraham <thomasinabraham@me.com>
2017-05-19 15:17:42 +1200

    Edit land1

--- client/components/Land1.jsx
+++ client/components/Land1.jsx
@@ -5,7 +5,7 @@ import {HashRouter as Router, Route, Link} from 'react-router-dom'
 const Land1 = () => {
 
 
-document.querySelector("body").style.backgroundImage = url("http://i.imgur.com/oIrFqeV.png")
+document.querySelector("body").style.backgroundImage = "url('http://i.imgur.com/oIrFqeV.png')"
 
 
   return(
