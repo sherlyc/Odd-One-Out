23e384e00b1d0cae51fb35fc1343615b351332a3
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-05-19 13:34:16 +1200

    push for merge

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import {HashRouter as Router, Route} from 'react-router-dom'
+import Land1 from './Land1'
+
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <Router>
+      <div>
+        <Route path='/' component={Land1}/>
+      </div>
+    </Router>
   )
 }
 
 export default App
-
